6cce9a4405a01ceef7e6d33b9693b172bd061e91
Alberto Muñoz <alberto.mqo@gmail.com>
2016-06-13 13:03:16 +0200

    Create custom service to share city query between controllers

--- Weather-Forecast-SPA/Starter/app.js
+++ Weather-Forecast-SPA/Starter/app.js
@@ -16,10 +16,19 @@ weatherApp.config(function($routeProvider) {
 
 });
 
-weatherApp.controller('homeController', ['$scope', function($scope) {
+weatherApp.service('cityService', function() {
+  this.city = "Barcelona, B";
+});
 
+weatherApp.controller('homeController', ['$scope', 'cityService',
+  function($scope, cityService) {
+    $scope.city = cityService.city;
+    $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+    });
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
-
+weatherApp.controller('forecastController', ['$scope', 'cityService',
+  function($scope, cityService) {
+    $scope.city = cityService.city;
 }]);
